1a1166c7edaa304cc6b37c4bd15658bf574c7286
Shanise Barona <shanisebarona@gmail.com>
2019-05-22 09:34:07 -0400

    updating

--- lib/sephora_scraper/scraper.rb
+++ lib/sephora_scraper/scraper.rb
@@ -1,15 +1,16 @@
-
+require 'pry'
+require 'watir'
+require 'nokogiri'
+require 'open-uri'
+require 'webdrivers'
 
 class Scraper
 
   def self.scrape_makeup
-    @@browser = Watir::Browser.new(:chrome)
-    @@browser.goto("https://www.sephora.com/beauty/new-beauty-products")
-    @@browser.divs(class: 'css-ix8km1')
+    browser = Watir::Browser.new(:chrome)
+    browser.goto("https://www.sephora.com/beauty/new-beauty-products")
+    browser.divs(class: 'css-ix8km1')
   end
 
-  def self.unmount
-    # Close browser
-    @@browser.close
-  end
-end
+
+end 
