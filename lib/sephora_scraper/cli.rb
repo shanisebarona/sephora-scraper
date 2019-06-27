d934e6f7734a18c5efe5a257dc369600d4753ad2
Shanise Barona <shanisebarona@gmail.com>
2019-06-27 07:02:33 -0400

    refactor: adds recursion in #selection, a teeny refactor is formatting for how products display in the list

--- lib/sephora_scraper/cli.rb
+++ lib/sephora_scraper/cli.rb
@@ -31,7 +31,7 @@ class SephoraScraper::CLI
     SephoraScraper::Beauty.all.each.with_index(1) do |product, index|
       input = gets.strip
       if input == index
-        puts "#{product.name} by #{product.brand}. #{product.price} #{product.description} #{product.uses} #{product.ingredients} #{product.about} #{product.rating}"
+        puts "#{product.name} - #{product.brand}. #{product.price} #{product.description} #{product.uses} #{product.ingredients} #{product.about} #{product.rating}"
       end
     end
   end
@@ -42,15 +42,16 @@ class SephoraScraper::CLI
       input = gets.strip.downcase
 
       case input 
-      when input.to_i > 0
+      when input.to_i > 0 # 1...20.
         #something, maybe a diff method all together
         #puts SephoraScraper::Beauty.all[input.to_i-1]
       when "list" 
         list_products
       when "exit", "quit"
-        #something
+        goodbye
       else 
-        puts "Oops, that was an inccorect selection. Type list or exit."
+        puts "Oops, that was an inccorect selection, please try again."
+        selection
       end
     end
 
